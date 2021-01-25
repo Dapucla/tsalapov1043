bbaf03570c1068304777f9011c13e08ad27956d9
qoterwater <marderics@gmail.com>
2021-01-25 11:38:35 +0300

    course

--- src/app/services/students.service.ts
+++ src/app/services/students.service.ts
@@ -5,7 +5,7 @@ import { BaseHttp } from './basehttp.service';
 @Injectable({
   providedIn: 'root'
 })
-export class StudentsService extends BaseHttp {
+export class StudentsDataBase extends BaseHttp {
 
   constructor(public http: HttpClient) {
     super(http, "students")
